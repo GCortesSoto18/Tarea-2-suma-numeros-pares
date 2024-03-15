2cf92c93e2fdb4d3d4f31b7da24a066804afd7c9
GCortesSoto18 <gcortes18@hotmail.com>
2024-03-14 22:01:54 -0600

    Tarea2

---
+++ sin_titulo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int i;
+	i = 1;
+	
+	while (i<=5){
+		if (i % 2 == 0) {
+			printf (" %d " , i);
+			
+		}
+		i++;
+		
+		
+	}
+		
+	printf (" La suma de los numeros pares es %d\n" , i);
+		
+	
+	return 0;
+}
+
